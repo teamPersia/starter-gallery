474078f559b1ddf979e837eb6d269c60dcbaaf05
ashayegh <shayegh.software@gmail.com>
2017-01-25 10:59:07 -0800

    small fix

---
+++ application/views/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>403 Forbidden</title>
+</head>
+<body>
+<header>
+            <span>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/gallery">Gallery</a>/li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/welcome">welcome</a></li>
+                </ul>
+            </span>
+</header>
+
+<p>Directory access is forbidden.</p>
+
+</body>
+</html>
